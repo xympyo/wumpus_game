f2b81f0159b51e1931d63cb871f36845b9be9cd2
XYMPyo <xynpyro@gmail.com>
2024-10-14 20:29:57 +0700

    feat : i dont even want to commit, but what if tomorrow my laptop broke?

--- index.js
+++ index.js
@@ -1,20 +1,79 @@
+// declaration
 let arrUp = document.getElementById(" arrUp");
 let arrLeft = document.getElementById("arrLeft");
 let arrRight = document.getElementById("arrRight");
 let arrDown = document.getElementById("arrDown");
-let SpawnRandomX = Math.floor(Math.random() * 4) + 1;
-let SpawnRandomY = Math.floor(Math.random() * 4) + 1;
-let xCoordinate = SpawnRandomX;
-let yCoordinate = SpawnRandomY;
-document
-  .getElementById("recty" + yCoordinate + "x" + xCoordinate)
-  .classList.add("active");
+// agent coordinate declaration
+let wumpusY;
+let wumpusX;
+let xCoordinate;
+let yCoordinate;
+// wumpus coordinate declaration
+let SpawnRandomX;
+let SpawnRandomY;
+// pit coordinate declaration
+let pitY;
+let pitX;
+// function create agent
+function createAgent() {
+  SpawnRandomX = Math.floor(Math.random() * 4) + 1;
+  SpawnRandomY = Math.floor(Math.random() * 4) + 1;
+  xCoordinate = SpawnRandomX;
+  yCoordinate = SpawnRandomY;
+  // spawning
+  document
+    .getElementById("recty" + yCoordinate + "x" + xCoordinate)
+    .classList.add("active");
+}
+// function create wumpus
+function createWumpus() {
+  // do until not the same as agent coordinate
+  do {
+    wumpusY = Math.floor(Math.random() * 4) + 1;
+    wumpusX = Math.floor(Math.random() * 4) + 1;
+  } while (wumpusY == yCoordinate && wumpusX == xCoordinate);
+  // spawning
+  document
+    .getElementById("recty" + wumpusY + "x" + wumpusX)
+    .classList.add("wumpus");
+}
+// function create pit
+function createPit() {
+  do {
+    pitY = Math.floor(Math.random() * 4) + 1;
+    pitX = Math.floor(Math.random() * 4) + 1;
+  } while (
+    (pitY == wumpusY && pitX == wumpusX) ||
+    (pitY == yCoordinate && pitX == xCoordinate)
+  );
+  // spawning
+  document.getElementById("recty" + pitY + "x" + pitX).classList.add("pit");
+  // return
+  return pitX, pitY;
+}
+// function start game create pit
+function startCreatePit() {
+  for (let i = 0; i < 3; i++) {
+    createPit();
+  }
+}
+// function start the game
+function startGame() {
+  createAgent();
+  createWumpus();
+  startCreatePit();
+}
+// game start
+startGame();
+// moving function
 function move(num) {
+  // moving
   const currentSquare = document.getElementById(
     "recty" + yCoordinate + "x" + xCoordinate
   );
   currentSquare.classList.remove("active");
   currentSquare.classList.add("passed");
+  // movement
   if (num == 1 && yCoordinate > 1) {
     yCoordinate--;
   } else if (num == 2 && xCoordinate > 1) {
